01176bd7f76cf8cf61a3306fcd5728e0dc52d80a
mozhavro <mozhario@gmail.com>
2017-06-30 00:35:15 +0300

    implemented CesarCipher tests

--- tests.py
+++ tests.py
@@ -1,16 +1,68 @@
 from unittest import TestCase
+from .cipher_cesar import CesarCipher
 
 
 class CesarCipherTest(TestCase):
 
     def test_encrypt(self):
-        pass
+        text = "abc"
+        rotate = 1
+
+        encrypted = CesarCipher.encrypt(text, rotate)
+        self.assertEqual(encrypted, "bcd")
+
+    def test_encrypt_cyclic(self):
+        """When the end of alphabet is reached, shift should be continued from it's start."""
+        text = "abcz"
+        rotate  = 1
+
+        encrypted = CesarCipher.encrypt(text, rotate)
+        self.assertEqual(encrypted, "bcda")
+
+    def test_encrypt_negative_rotate(self):
+        """Shift back"""
+        text = "bcz"
+        rotate  = -1
+
+        encrypted = CesarCipher.encrypt(text, rotate)
+        self.assertEqual(encrypted, "aby")
+
+    def test_encrypt_negative_rotate_cyclic(self):
+        """For the negative rotate, when the start of alphabet is reached, shift should be continued from it's end."""
+        text = "abcz"
+        rotate  = -1
+
+        encrypted = CesarCipher.encrypt(text, rotate)
+        self.assertEqual(encrypted, "zaby")
+
+    def test_encrypt_text_with_whitespaces(self):
+        """Whitespaces should be left untouched"""
+        text = "abc bcd zzz"
+        rotate = 1
+
+        encrypted = CesarCipher.encrypt(text, rotate)
+        self.assertEqual(encrypted, "bcd cde aaa")
 
     def test_decrypt(self):
-        pass
+        """In case of cesar cipher, decryption should mean rotation of characters to negative value of rotation key."""
+        text = "bcd"
+        rotate = 1
+
+        encrypted = CesarCipher.decrypt(text, rotate)
+        self.assertEqual(encrypted, "abc")
 
     def test_characters_validation(self):
-        pass
+        """Everything except alphabetical characters and whitespaces is invalid"""
+        invalid_input = "fnds323254s"
+        with self.assertRaises(ValueError):
+            rotated = CesarCipher(invalid_input, 1)
 
     def test_sanity(self):
-        pass
+        """Encryption + decription should keep the original text"""
+        original_text = "bcd"
+        rotate = 1
+
+        encrypted = CesarCipher.encrypt(original_text, rotate)
+        decrypted = CesarCipher.decrypt(encrypted, rotate)
+
+        self.assertEqual(original_text, decrypted)
