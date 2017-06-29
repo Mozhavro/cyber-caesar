81dbf8266dc0511f92485f43ecf179c926689fee
mozhavro <mozhario@gmail.com>
2017-06-29 23:38:15 +0300

    prototyped tests

---
+++ tests.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+
+class CesarCipherTest(TestCase):
+
+    def test_encrypt(self):
+        pass
+
+    def test_decrypt(self):
+        pass
+
+    def test_characters_validation(self):
+        pass
+
+    def test_sanity(self):
+        pass
