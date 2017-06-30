0a693a07ab562797c21582fb43b39478d352dfa5
mozhavro <mozhario@gmail.com>
2017-06-30 09:51:00 +0300

    reduced some alphabet responsibilities (left one sequence of chars, no uppercase)

--- alphabet.py
+++ alphabet.py
@@ -1,23 +1,18 @@
 class Alphabet:
     """Cyclic iterable class, that represents alphabet."""
+
     def __init__(self, start, end, up_start=None, up_end=None, other=None):
         """Alphabet is represented by the bounds of characters ASCII codes,
         not the sequences of characters:
         start - alphabet first character
         end - alphabet last character
-        up_start - uppercase alphabet first character (optional)
-        up_end - uppercase alphabet last character (optional)
         other - any characters you want! list of attitional characters"""
         self.start = ord(start)
         self.end = ord(end)
-        self.up_start = ord(up_start)
-        self.up_end = ord(up_end)
         self.other = [ord(char) for char in other]
 
     def get_all_characters_codes(self):
-        return list(range(self.start, self.end)) + \
-                list(range(self.up_start, self.up_end)) + \
-                self.other
+        return list(range(self.start, self.end)) + self.other
 
     def get_all_characters(self):
         codes = self.get_all_characters_codes()
