057db13802b085e7369f9b40e93dcea113e5809f
mozhavro <mozhario@gmail.com>
2017-07-04 01:59:18 +0300

    fixed staticfiles configuration

--- caesar/settings.py
+++ caesar/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
+    '0.0.0.0',
     'cybercaesar.herokuapp.com'
 ]
 
@@ -124,10 +125,10 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
 )
--- caesar/urls.py
+++ caesar/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('caesar_cypher.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
